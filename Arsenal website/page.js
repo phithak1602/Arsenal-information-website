083699078d4e54c82f2a8bac6fb5c25e0a66cf3f
Phithak Boonnoi <gtaninphitak@gmail.com>
2022-06-23 21:19:47 +0700

    Miniproject

---
+++ Arsenal website/page.js
@@ -0,0 +1,13 @@
+function page1(){
+    window.location.assign("./home.html")
+}
+function page2(){
+    window.location.assign("./index.html")
+}
+function pagefacebook(){
+    window.location.assign("https://www.facebook.com/phithak.boonnoi/")
+}
+
+function pageig(){
+    window.location.assign("https://www.instagram.com/phithak_16/")
+}
